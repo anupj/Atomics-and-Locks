58cb0c860a33e90fc5fb8f6d0cdb732b2b1c38c9
Anup Jadhav <anup.jadhav@gmail.com>
2023-04-30 16:59:50 +0200

    Chapter 03 continued

---
+++ chapter-03/mem_ordering_06/src/main.rs
@@ -0,0 +1,23 @@
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::*;
+use std::thread;
+
+static mut DATA: String = String::new();
+static LOCKED: AtomicBool = AtomicBool::new(false);
+
+fn f() {
+    if LOCKED.compare_exchange(false, true, Acquire, Relaxed).is_ok() {
+        // Safety: We hold the exclusive lock, so nothing else is accessing DATA.
+        unsafe { DATA.push('!') };
+        LOCKED.store(false, Release);
+    }
+}
+
+fn main() {
+    thread::scope(|s| {
+        for _ in 0..100 {
+            s.spawn(f);
+        }
+    });
+    unsafe { println!("DATA is now {:?}", DATA) };
+}
